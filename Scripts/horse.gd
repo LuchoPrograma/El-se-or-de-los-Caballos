4d5af014e0470f931d87e9bb8ff74c309547837d
Luciano Federicci <lucianofedericci@gmail.com>
2025-08-31 03:35:06 -0300

    minor improvments

--- Scripts/horse.gd
+++ Scripts/horse.gd
@@ -8,24 +8,19 @@ func _physics_process(delta: float) -> void:
 	
 	if state == "drowned":
 		rotation += 0.1
-		
-
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	state = "drowned"
-	$CollisionShape2D.disabled = true
+	$Collision.disabled = true
+	collision_mask = 0
+	collision_layer = 0
 	velocity.y = 0
-	print()
 	if body == $Main/Aguas/ola_idquierda:
-		
 		velocity.x = 100
-		
 	elif body == $Main/Aguas/ola_derecha:
 		velocity.x = -100
-	else:
-		print("wrong nodes b")
 		
 	await get_tree().create_timer(1).timeout
 	queue_free()
